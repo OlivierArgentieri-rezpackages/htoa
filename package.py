2ac3027f2a18687c1e5ac7c69147977877b3d2fc
OlivierArgentieri <olivierargentieri@gmail.com>
2021-10-27 18:31:52 +0200

    Create package.py

---
+++ package.py
@@ -0,0 +1,11 @@
+name = "htoa"
+version = "5.6.3.0"
+
+requires = [
+    "houdini",
+]
+
+def commands():
+    env.PATH.append(r"F:\Program_Files\Houdini_18.5.596\htoa\htoa-5.6.3.0_ra766b1f_houdini-18.5.596.py3/scripts/bin")
+    env.HOUDINI_PATH.append(r"F:\Program_Files\Houdini_18.5.596\htoa\htoa-5.6.3.0_ra766b1f_houdini-18.5.596.py3")
+    env.solidangle_LICENSE.append("5053@localhost")
